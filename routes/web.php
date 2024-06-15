b0704367fb5ab742d7f07ec546f3a694f611fe0b
RKP <ajarubab@gmail.com>
2024-06-15 17:37:27 +0530

    Routing types to render a view page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+//---------------------- Methods to render a view page ----------------------------------
+
 Route::view('/home','home');
+
+Route::get('/home', function() {
+    return view('home');
+});
+
+Route::get('/home',[HomeController::class,'show']);
+
+//------------------------------------------------------------------------------------------
