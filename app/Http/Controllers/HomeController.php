7b7d4e773bffb994a05a2372c097b013c2444065
RKP <ajarubab@gmail.com>
2024-06-22 12:54:58 +0530

    grouped routes of same controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,10 +7,22 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     function show() {
-        return "Showing Student";
+        return "Showing Student details";
     }
 
     function add() {
-        return view('about');
+        return "Adding Student details";
+    }
+    
+    function delete() {
+        return "Student Deleted.";
+    }
+
+    function update() {
+        return 'Updating Student details.';
+    }
+    
+    function about($name) {
+        return $name;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::view('/home','home');
-Route::view('/about','about');
-
-
-// Route::view('/naam', 'user', ["name" => "ramesh"]);
-// Route::view('student/home', 'home');
 // Route::get('student/show',[HomeController::class,'show']);
 // Route::get('student/add',[HomeController::class,'add']);
-// Route::get('/home/{name?}', function($name = null) {
-//     return view('user',['name' => $name]);
-// });
+// Route::get('student/delete',[HomeController::class,'delete']);
+// Route::get('student/update',[HomeController::class,'update']);
+// Route::get('about/{name}',[HomeController::class,'about']);
 
-    
-Route::prefix('student')->group(function(){
-    Route::view('/naam', 'user', ["name" => "ramesh"]);
-    Route::view('/home', 'home');
-    Route::get('/show',[HomeController::class,'show']);
-    Route::get('/add',[HomeController::class,'add']);
-    Route::get('/home/{name?}', function($name = null) {
-        return view('user',['name' => $name]);
-    });
+Route::controller(HomeController::class)->group(function(){
+    Route::get('show','show');
+    Route::get('add','add');
+    Route::get('delete','delete');
+    Route::get('update','update');
+    Route::get('about/{name}','about');
 });
