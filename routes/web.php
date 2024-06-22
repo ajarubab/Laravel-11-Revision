eb5a8b57fa05ae38a81298089a83a494c88b6523
RKP <ajarubab@gmail.com>
2024-06-22 13:26:14 +0530

    grouping of prefix and same controller routes

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-// Route::get('student/show',[HomeController::class,'show']);
-// Route::get('student/add',[HomeController::class,'add']);
-// Route::get('student/delete',[HomeController::class,'delete']);
-// Route::get('student/update',[HomeController::class,'update']);
-// Route::get('about/{name}',[HomeController::class,'about']);
-
-Route::controller(HomeController::class)->group(function(){
-    Route::get('show','show');
-    Route::get('add','add');
-    Route::get('delete','delete');
-    Route::get('update','update');
-    Route::get('about/{name}','about');
+Route::prefix('student')->group(function(){
+    Route::controller(HomeController::class)->group(function(){
+        Route::get('show','show');
+        Route::get('add','add');
+        Route::get('delete','delete');
+        Route::get('update','update');
+        Route::get('about/{name}','about');
+    });
 });
