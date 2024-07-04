789aa670cdc2cd66f949ae014a4cf94a50b956a7
RKP <ajarubab@gmail.com>
2024-07-04 11:16:41 +0530

    connecting non-matching table name with a particular model and fetching values of table

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,8 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
-    
+
     // this file is created using the command " php artisan make:model Student "
-    // Normally the name of the model should be Same as of table name with first letter in capital case.
-    // Normally the name of the model should be Singular of table name of the database.
+
+    // 1. Normally the name of the model should be Same as of table name with first letter in capital case.
+    // 2. Normally the name of the model should be Singular of table name of the database.
+
+    // If the model is not created using above 2 rules then we can connect our model by following way:
+    // just assign the table name to the protected $table variable and we are good to go.
+
+    protected $table = "college_students";  // here 'college_students' is the table name in the database.
 }
