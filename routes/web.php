339df7a80a8341ae737cdce425ed4bb7960a4505
RKP <ajarubab@gmail.com>
2024-07-08 11:14:58 +0530

    clearing all for new ones

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\FormController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::view('form','form');
-Route::post('/user',[FormController::class,'postRoute']);
 
 
 
